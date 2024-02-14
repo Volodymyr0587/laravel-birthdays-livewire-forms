f58c0b0bc79b9eaa814dfa4bda7c6483cfefa04f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-02-14 17:27:04 +0200

    Create database; change migrations; Seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(3)
+        ->state(new Sequence(
+            ['name' => 'Mary', 'email' => 'mary@mary-ui.com', 'dob' => '2000-06-20'],
+            ['name' => 'Giovanna', 'email' => 'giovanna@mary-ui.com', 'dob' => '2017-03-10'],
+            ['name' => 'Marina', 'email' => 'marina@mary-ui.com', 'dob' => '2019-01-01'],
+        ))
+        ->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
